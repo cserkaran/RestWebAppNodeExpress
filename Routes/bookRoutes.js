855f8bc6efee5e772a4f77de4f9145155748f848
Karan Sharma <sharmak@crossmuller.com.au>
2017-07-30 20:15:16 +1000

    Covered upto module 5. Remove verb implemented.

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -32,14 +32,58 @@ var routes = function(Book) {
             });
         });
 
+    bookRouter.use('/Books/:bookId', function(req, res, next) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if (err) {
+                res.status(500).send(err);
+            } else if (book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book found');
+            }
+        });
+    });
+
     bookRouter.route('/Books/:bookId')
         .get(function(req, res) {
-
-            Book.findById(req.params.bookId, function(err, books) {
+            res.json(req.book);
+        })
+        .put(function(req, res) {
+            var book = req.book;
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.genre = req.body.genre;
+            book.read = req.body.read;
+            book.save(function(err) {
                 if (err) {
                     res.status(500).send(err);
                 } else {
-                    res.json(books);
+                    res.json(req.book);
+                }
+            });
+        }).patch(function(req, res) {
+            if (req.body._id) {
+                delete(req.body._id);
+            }
+
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+
+            req.book.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        }).delete(function(req, res) {
+            req.book.remove(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('Removed');
                 }
             });
         });
